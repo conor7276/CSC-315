9713a80dc84df5a053c1584b2860be5fea30852a
conor7276 <conor7276@gmail.com>
2023-03-10 14:50:20 -0500

    fixed hw6

--- Homework/Homework6/q2.sql
+++ Homework/Homework6/q2.sql
@@ -1,4 +1,5 @@
-SELECT * FROM(
+SELECT Id, FirstName, LastName, BirthDate, RegionId, RegionDescription
+ FROM(
     SELECT DISTINCT Employee.Id, Employee.FirstName, Employee.LastName, Employee.BirthDate, Territory.RegionId, Region.RegionDescription,
     dense_rank() OVER(Partition BY Region.Id ORDER BY Employee.BirthDate DESC) 'Rank'
     FROM Employee, EmployeeTerritory, Territory, Region
@@ -8,11 +9,11 @@ WHERE t.rank <= 3
 
 
 -- C:\Users\conor\OneDrive\Desktop\school\2023 Spring\CSC-315\Homework\Homework6>sqlite3 northwind.db ".read q2.sql"
--- 5|Steven|Buchanan|1987-03-04|1|Eastern|1
--- 2|Andrew|Fuller|1984-02-19|1|Eastern|2
--- 1|Nancy|Davolio|1980-12-08|1|Eastern|3
--- 6|Michael|Suyama|1995-07-02|2|Western|1
--- 7|Robert|King|1992-05-29|2|Western|2
--- 9|Anne|Dodsworth|1998-01-27|3|Northern|1
--- 8|Laura|Callahan|1990-01-09|3|Northern|2
--- 3|Janet|Leverling|1995-08-30|4|Southern|1
+-- 5|Steven|Buchanan|1987-03-04|1|Eastern
+-- 2|Andrew|Fuller|1984-02-19|1|Eastern  
+-- 1|Nancy|Davolio|1980-12-08|1|Eastern  
+-- 6|Michael|Suyama|1995-07-02|2|Western 
+-- 7|Robert|King|1992-05-29|2|Western    
+-- 9|Anne|Dodsworth|1998-01-27|3|Northern
+-- 8|Laura|Callahan|1990-01-09|3|Northern
+-- 3|Janet|Leverling|1995-08-30|4|Southern
